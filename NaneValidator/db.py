a259dac2a4215b0946abc0f73516442630a1da03
Bodia2cat <62301981+Bodia2cat@users.noreply.github.com>
2020-04-26 10:44:04 +0300

    Add files via upload

---
+++ NaneValidator/db.py
@@ -0,0 +1,1 @@
+list_p = ["Bodia_2cat", "Test", "Hamster_3000", "Your_Teacher", "dad", "god_of_money", "Lol", "mum"]
---
+++ NaneValidator/validator.py
@@ -0,0 +1,32 @@
+from db import list_p
+while_b = "True"
+while while_b == "True":
+	name = input("Enter name: ")
+	lenth = len(list_p)
+	if list_p[0] == name:
+		print("it name is not good")
+		
+	if list_p[1] == name:
+		print("it name is not good")
+		
+	if list_p[2] == name:
+		print("it name is not good")
+		#exit()
+	if list_p[3] == name:
+		print("it name is not good")
+		#exit()
+	if list_p[4] == name:
+		print("it name is not good")
+		#exit()
+	if list_p[5] == name:
+		print("it name is not good")
+		#exit()
+	if list_p[6] == name:
+		print("it name is not good")
+		#exit()
+	if list_p[7] == name:
+		print("it name is not good")
+		#exit()
+	else:
+		print("Your name is good!")
+		#exit()
